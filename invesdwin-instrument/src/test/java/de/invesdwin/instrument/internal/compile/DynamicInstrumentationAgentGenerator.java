b692214793c5d32d77c2ae9c1477d8724b57aa8c
subes <gsubes@gmail.com>
2021-09-20 14:17:13 +0200

    java 8 fix

--- invesdwin-instrument/src/test/java/de/invesdwin/instrument/internal/compile/DynamicInstrumentationAgentGenerator.java
+++ invesdwin-instrument/src/test/java/de/invesdwin/instrument/internal/compile/DynamicInstrumentationAgentGenerator.java
@@ -17,7 +17,7 @@ public class DynamicInstrumentationAgentGenerator {
     @Test
     public void test() throws IOException {
         final File folder = new File(
-                "src/main/java/" + DynamicInstrumentationAgent.class.getPackageName().replace(".", "/"));
+                "src/main/java/" + DynamicInstrumentationAgent.class.getPackage().getName().replace(".", "/"));
         final String template = org.apache.commons.io.FileUtils.readFileToString(
                 new File(folder, DynamicInstrumentationAgentCompiler.TEMPLATE), Charset.defaultCharset());
         for (int i = DynamicInstrumentationAgentCompiler.FIRST_PRECOMPILED_UUID; i <= DynamicInstrumentationAgentCompiler.MAX_PRECOMPILED_UUID; i++) {
