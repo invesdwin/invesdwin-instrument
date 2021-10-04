6ac388eb85e73dde8f3a2479d432871278b4b8de
subes <gsubes@gmail.com>
2021-10-04 18:25:13 +0200

    explicit fqdn

--- tests/springboot-devtools-testapp/src/main/java/com/sample/api/server/Application.java
+++ tests/springboot-devtools-testapp/src/main/java/com/sample/api/server/Application.java
@@ -4,7 +4,6 @@ import java.lang.instrument.Instrumentation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import org.aspectj.weaver.loadtime.Agent;
 import org.burningwave.core.assembler.StaticComponentContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -50,7 +49,7 @@ public class Application extends SpringBootServletInitializer {
 		 * make sure aspectj-LTW is loaded also into
 		 * org.springframework.boot.devtools.restart.RestartLauncher classloader
 		 */
-		Agent.agentmain("", InstrumentationSavingAgent.getInstrumentation());
+		org.aspectj.weaver.loadtime.Agent.agentmain("", InstrumentationSavingAgent.getInstrumentation());
 		// test that the aspect works fine
 		new TestAspectUseCase().testAspect();
 
